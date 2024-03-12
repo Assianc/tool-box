bd46ce469c13c77973790c4fa3ee93dbb56d19af
zhoubinxin <binxin.zhou@outlook.com>
2024-03-12 16:27:25 +0800

    feat: gemini pro api

---
+++ chat/gemini.py
@@ -0,0 +1,42 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
+def gemini(content):
+    load_dotenv()
+    key = os.environ['GEMINI_KEY']
+    # 定义请求的 URL
+    url = f"https://gemini.bxin.top/v1beta/models/gemini-pro:generateContent?key={key}"
+    print(url)
+
+    # 定义要发送的 JSON 数据
+    data = {
+        "contents": [
+            {
+                "parts": [
+                    {
+                        "text": content
+                    }
+                ]
+            }
+        ]
+    }
+
+    # 定义请求头
+    headers = {
+        "Content-Type": "application/json"
+    }
+
+    # 发送 POST 请求
+    response = requests.post(url, json=data, headers=headers)
+
+    # 输出响应结果
+    return response.json()
+
+
+if __name__ == '__main__':
+    content = "帮我写一篇关于人工智能的文章"
+    result = gemini(content)
+    print(result)
