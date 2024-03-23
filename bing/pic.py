6d983ea91d2e737499f6927f8627c81582189341
zhoubinxin <binxin.zhou@outlook.com>
2024-03-23 19:10:43 +0800

    perf: 完善获取bing壁纸代码

--- bing/pic.py
+++ bing/pic.py
@@ -4,39 +4,44 @@ import os
 from dotenv import load_dotenv
 
 
-def get_daily_image():
-    daily_image_url = 'https://global.bing.com/HPImageArchive.aspx?format=js&idx=0&n=9&pid=hp&FORM=BEHPTB&uhd=1&uhdwidth=3840&uhdheight=2160&setmkt=zh_cn&setlang=zh'
-    try:
-        r = requests.get(daily_image_url)
-        r = r.json()
-        return 'https://global.bing.com/' + r['images'][0]['url']
-    except Exception as e:
-        print(f'bing获取壁纸失败：{e}')
-
-
 class ImageDriver:
     def __init__(self, driver='bing'):
         self.driver = driver
 
     def get_image_url(self):
+        """
+        获取图片链接
+        :return:
+        """
         if self.driver == 'bing':
-            return get_daily_image()
+            return self.get_daily_image()
         return None
 
+    def get_daily_image(self):
+        """
+        获取每日一图
+        :return:
+        """
+        daily_image_url = ('https://global.bing.com/HPImageArchive.aspx?format=js&idx=0&n=9&pid=hp&FORM=BEHPTB&uhd=1'
+                           '&uhdwidth=3840&uhdheight=2160&setmkt=zh_cn&setlang=zh')
+        try:
+            r = requests.get(daily_image_url)
+            r = r.json()
+            return 'https://global.bing.com/' + r['images'][0]['url']
+        except Exception as e:
+            print(f'获取{self.driver}壁纸失败：{e}')
 
-if __name__ == '__main__':
-    # 读取.env文件中的变量
+
+def main():
     load_dotenv()
     try:
-        driver = os.environ['DRIVER']  # bing
+        driver = os.environ['DRIVER']
     except Exception as e:
         print(f'获取DRIVER失败：{e}')
         driver = 'bing'
 
     image_driver = ImageDriver(driver)
     image_url = image_driver.get_image_url()
-    # https://global.bing.com//th?id=OHR.BambooPanda_ZH-CN8455481760_UHD.jpg&rf=LaDigue_UHD.jpg&pid=hp&w=3840&h=2160&rs=1&c=4
-    # 下载图片，保存到当前目录下的images文件夹，文件名为链接上的id
     if image_url:
         image_id = image_url.split('id=')[1].split('_')[0]
         image_path = f'images/{image_id}.jpg'
@@ -47,3 +52,7 @@ if __name__ == '__main__':
             print(f'壁纸已存在：{image_path}')
     else:
         print('获取壁纸失败')
+
+
+if __name__ == '__main__':
+    main()
