97259c10fdd35d086f0cc081a324851ecf0bb6df
zhoubinxin <binxin.zhou@outlook.com>
2024-03-24 15:48:55 +0800

    feat: pdf和word相互转换

--- office/pdf2word.py
+++ office/pdf2word.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from pdf2docx import parse
+from docx2pdf import convert
 
 
 def convert_to_docx(file_path):
@@ -19,4 +20,32 @@ def convert_to_docx(file_path):
         return None
 
 
-convert_to_docx(sys.argv[1])
+def convert_to_pdf(file_path):
+    directory = os.path.dirname(file_path)
+    file = os.path.basename(file_path)
+    file_name = os.path.splitext(file)[0]
+    file_extension = os.path.splitext(file)[1]
+
+    if file_extension == ".docx":
+        docx_file_path = os.path.join(directory, file)
+        pdf_file_path = os.path.join(directory, f"{file_name}.pdf")
+        convert(docx_file_path, pdf_file_path)
+        print(f"Successfully converted '{file}' to '{file_name}.pdf'.")
+        return pdf_file_path
+    else:
+        return None
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+        file_extension = os.path.splitext(file_path)[1]
+
+        if file_extension == ".pdf":
+            convert_to_docx(file_path)
+        elif file_extension == ".docx":
+            convert_to_pdf(file_path)
+        else:
+            print("Unsupported file format. Please provide a PDF or DOCX file.")
+    else:
+        print("Please provide a file path as a command-line argument.")
