3a070baf414bcbba749b8ac1ee1a6f8e2cd16dfe
zhoubinxin <binxin.zhou@outlook.com>
2024-03-31 21:27:24 +0800

    feat: 支持多个Gemini API

--- chat/gemini.py
+++ chat/gemini.py
@@ -1,41 +1,55 @@
 import os
+from random import random
 
 import requests
 from dotenv import load_dotenv
 
 
-def gemini(content):
-    load_dotenv()
-    key = os.environ['GEMINI_KEY']
-    # 定义请求的 URL
-    url = f"https://gemini.bxin.top/v1beta/models/gemini-pro:generateContent?key={key}"
+class Gemini(object):
+    @classmethod
+    def chat(cls, prompt):
+        load_dotenv()
+        keys = os.environ['GEMINI_KEY'].split(',')
 
-    # 定义要发送的 JSON 数据
-    data = {
-        "contents": [
-            {
-                "parts": [
-                    {
-                        "text": content
-                    }
-                ]
-            }
-        ]
-    }
+        # 定义要发送的 JSON 数据
+        data = {
+            "contents": [
+                {
+                    "parts": [
+                        {
+                            "text": prompt
+                        }
+                    ]
+                }
+            ]
+        }
 
-    # 定义请求头
-    headers = {
-        "Content-Type": "application/json"
-    }
+        # 定义请求头
+        headers = {
+            "Content-Type": "application/json"
+        }
 
-    # 发送 POST 请求
-    response = requests.post(url, json=data, headers=headers)
+        response = None
 
-    # 输出响应结果
-    return response.json()
+        for i in range(len(keys)):
+            key = keys[i]
+            # 定义请求的 URL
+            url = f"https://gemini.bxin.top/v1beta/models/gemini-pro:generateContent?key={key}"
+
+            # 发送 POST 请求
+            response = requests.post(url, json=data, headers=headers)
+            response = response.json()
+            try:
+                response = response['candidates'][0]['content']['parts'][0]['text']
+                break
+            except Exception as e:
+                print(f"Error: {e}")
+                response = 'error'
+
+        return response
 
 
 if __name__ == '__main__':
     content = "明天是周末，没有课，使用作为写一段庆祝的话"
-    result = gemini(content)
+    result = Gemini.chat(content)
     print(result)
