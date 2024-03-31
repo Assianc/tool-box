0fa4ebba99ead906010bbd9e217b47209f1d1a0b
zhoubinxin <binxin.zhou@outlook.com>
2024-03-31 22:20:45 +0800

    feat: 企业微信推送

--- message/message.py
+++ message/message.py
@@ -10,6 +10,12 @@ class Messages(object):
 
     @classmethod
     def push_plus(cls, **kwargs):
+        # message = {
+        #     "title": f"天翼云签到推送",
+        #     "content": content,
+        #     "template": "markdown",
+        #     "channel": "wechat"
+        # }
         pushplush = os.environ["PUSHPLUS_TOKEN"]
         url = 'http://www.pushplus.plus/send'
         data = {
@@ -23,3 +29,22 @@ class Messages(object):
         headers = {'Content-Type': 'application/json'}
         requests.post(url, data=body, headers=headers)
         print("send push plus success")
+
+    @classmethod
+    def wechat(cls, **kwargs):
+        # message={
+        #     "msgtype": "markdown",
+        #     "content": content
+        #}
+        load_dotenv()
+        webhook_url = os.environ["QYAPI"]
+        headers = {'Content-Type': 'application/json'}
+        payload = {
+            "msgtype": kwargs.get("msgtype"),
+            kwargs.get("msgtype"): {
+                "content": kwargs.get("content")
+            }
+        }
+
+        response = requests.post(webhook_url, headers=headers, data=json.dumps(payload))
+        print(response.text)
